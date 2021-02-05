1503140855e5d3afbfda4b6232a2dea0874b6f90
nik-lazer <nick.lazarev@gmail.com>
2021-02-05 16:34:31 +0300

    Add spring-based consumer

---
+++ contract-consumer/src/main/kotlin/lan/guild/contractconsumer/ContractConsumerApplication.kt
@@ -0,0 +1,11 @@
+package lan.guild.contractconsumer
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ContractConsumerApplication
+
+fun main(args: Array<String>) {
+	runApplication<ContractConsumerApplication>(*args)
+}
---
+++ contract-consumer/src/test/kotlin/lan/guild/contractconsumer/ContractConsumerApplicationTests.kt
@@ -0,0 +1,13 @@
+package lan.guild.contractconsumer
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ContractConsumerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
