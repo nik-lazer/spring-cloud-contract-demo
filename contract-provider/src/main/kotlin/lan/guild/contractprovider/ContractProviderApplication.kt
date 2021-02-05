634c352f694bcc5feedd23a2d0146c786bb6c40c
nik-lazer <nick.lazarev@gmail.com>
2021-02-05 16:11:20 +0300

    Add service-producer

---
+++ contract-provider/src/main/kotlin/lan/guild/contractprovider/ContractProviderApplication.kt
@@ -0,0 +1,11 @@
+package lan.guild.contractprovider
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ContractProviderApplication
+
+fun main(args: Array<String>) {
+	runApplication<ContractProviderApplication>(*args)
+}
---
+++ contract-provider/src/test/kotlin/lan/guild/contractprovider/ContractProviderApplicationTests.kt
@@ -0,0 +1,13 @@
+package lan.guild.contractprovider
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ContractProviderApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
