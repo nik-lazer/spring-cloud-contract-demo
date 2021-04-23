7d4b85f33f4d94fe678ef2660d3cbd78b11e2a12
nik-lazer <nick.lazarev@gmail.com>
2021-04-23 19:32:40 +0300

    add preparing stubs as artefact

--- contract-provider/build.gradle.kts
+++ contract-provider/build.gradle.kts
@@ -1,10 +1,10 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "2.4.2"
-	id("io.spring.dependency-management") version "1.0.11.RELEASE"
-	kotlin("jvm") version "1.4.21"
-	kotlin("plugin.spring") version "1.4.21"
+    id("org.springframework.boot") version "2.4.2"
+    id("io.spring.dependency-management") version "1.0.11.RELEASE"
+    kotlin("jvm") version "1.4.21"
+    kotlin("plugin.spring") version "1.4.21"
     id("org.springframework.cloud.contract") version "3.0.1"
 }
 
@@ -13,40 +13,51 @@ version = "0.0.1-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_11
 
 repositories {
-	mavenCentral()
+    mavenCentral()
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testImplementation("org.springframework.cloud:spring-cloud-starter-contract-verifier:3.0.1")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.cloud:spring-cloud-starter-contract-verifier:3.0.1")
 }
 
 contracts {
-	testFramework.set(org.springframework.cloud.contract.verifier.config.TestFramework.JUNIT5)
-	baseClassForTests.set("lan.guild.contractprovider.contract.BaseContractTest")
+    testFramework.set(org.springframework.cloud.contract.verifier.config.TestFramework.JUNIT5)
+    baseClassForTests.set("lan.guild.contractprovider.contract.BaseContractTest")
 }
 
 tasks.withType<KotlinCompile> {
-	kotlinOptions {
-		freeCompilerArgs = listOf("-Xjsr305=strict")
-		jvmTarget = "11"
-	}
+    kotlinOptions {
+        freeCompilerArgs = listOf("-Xjsr305=strict")
+        jvmTarget = "11"
+    }
 }
 
 tasks.withType<Test> {
-	useJUnitPlatform()
+    useJUnitPlatform()
 
-	testLogging {
-		exceptionFormat = org.gradle.api.tasks.testing.logging.TestExceptionFormat.FULL
-	}
+    testLogging {
+        exceptionFormat = org.gradle.api.tasks.testing.logging.TestExceptionFormat.FULL
+    }
+}
+
+tasks.register<Copy>("copyContractArtefact") {
+    from(layout.buildDirectory.dir("libs"))
+    into(layout.buildDirectory.dir("libs"))
+
+    rename("(.+)-stubs(.+)", "artefact-$1$2")
+}
+
+tasks.named("verifierStubsJar") {
+        finalizedBy("copyContractArtefact")
 }
 
 sourceSets {
-	test {
-		java.srcDir("src/test/kotlin")
-	}
+    test {
+        java.srcDir("src/test/kotlin")
+    }
 }
